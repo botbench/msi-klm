5c9c92af48550ec51fa2bfe8938aa1b531119fcb
Xander <Xander Soldaat>
2014-06-16 07:18:39 +0200

    Initial commit with Eclipse project files

---
+++ src/msi-klm.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : msi-klm.c
+ Author      : Xander Soldaat
+ Version     :
+ Copyright   : Copyright 2014 Xander Soldaat
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello MSI-KLM"); /* prints Hello MSI-KLM */
+	return EXIT_SUCCESS;
+}
